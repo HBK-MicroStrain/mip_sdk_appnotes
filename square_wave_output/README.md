0e5d2faca5d725308c987b8406983870d1336ecf
microstrain-sam <105877710+microstrain-sam@users.noreply.github.com>
2025-07-10 17:35:51 -0400

    Initial commit; one example

---
+++ square_wave_output/README.md
@@ -0,0 +1,117 @@
+
+Synchronized Square Wave Output
+===============================
+
+This is the companion project for the application note
+[Syncrhonized Square Wave Output](https://s3.amazonaws.com/files.microstrain.com/CV7+Online/user_manual_content/app_notes/Example%20Square%20Wave%20Output.htm)
+
+
+This project will configure the device to output a square wave on one of the GPIO pins using the event system.
+The square wave will be synchronized with either reference or external time.
+
+
+The GPIO action will control the state of the pin based on the trigger status.
+The threshold trigger will monitor one of the available timestamp data quantities and continuously
+compare it against a recurring interval. When the time is between the start of the interval and the threshold value,
+the trigger will be activated, and when the time is between the threshold and the end of the interval, the trigger will
+be deactivated. This sequence then repeats for the next interval.
+
+For example, if the interval is set to 10 seconds, and the threshold to 4 seconds, the trigger will
+be active when the time is between 0-3, 10-13, 20-23, etc. seconds, and inactive between 4-9, 14-19, 24-29, etc. seconds.
+The pin will be high during the active times and low otherwise, thus producing a square wave with 40% duty cycle. 
+
+
+Operation
+---------
+
+### Initial Setup
+* Create & open the connection
+* Create the device interface
+* Ensure the device is idle and communicates
+* Clear any existing events and GPIO configurations.
+
+### Application Configuration
+These steps mirror those found in the application note.
+
+* Configure the GPIO pin for GPIO_OUTPUT mode
+* Assign a GPIO action to control the pin
+* Assign a threshold trigger in interval mode
+* Enable the trigger
+
+### Application Run Loop
+For demonstration & debug purposes, this project will continuously monitor the trigger status.
+This gives a visual representation of the behavior directly in the terminal, without any additional
+hardware besides the 3DM device itself.
+
+If the configured frequency is less than 5 Hz, a verbose output mode is used. It prints
+lines like this every 100 ms (10 Hz):
+
+`[INFO ]: Trigger 1 status:  [ ] Active  [X] Enabled  [ ] Testing`
+
+Each `[ ]` will have an `X` if the corresponding flag is set.
+
+If the configured frequency is more than 5 Hz, a high-speed output is used. It prints
+one `+` or `-` every millisecond, depending on the trigger's active state. Output will
+look similar to this:
+
+```
+--------++++++++---------++++++++--------+++++++++--------++++++++---------++++++++--------+++++++++
+--------++++++++---------++++++++--------+++++++++--------++++++++---------++++++++--------+++++++++
+--------++++++++---------++++++++--------+++++++++--------++++++++---------++++++++--------+++++++++
+```
+
+This graphs the trigger's state over time, giving a rough estimate of the period and duty cycle.
+Each character roughly represents a sample roughly every millisecond. The example above is for a
+60 Hz square wave, so there are groups of 8 `+` and 8 `-`. Every third group has an extra one
+since 60 Hz is a period of 16.667 ms, which isn't an integral number of ms.
+
+(!) CAUTION: This is only intended for demo and debugging purposes. You should not rely on it nor assume
+the timing is accurate from this display alone. In particular, it is sensitive to your PC's performance.
+Trigger status cannot be streamed, only polled, so it is subject to significant jitter.
+Always check the hardware with a suitable instrument like an oscilloscope or frequency counter.
+
+
+Configuration Options
+---------------------
+
+These values are defined at the top of the main file for clarity and to permit easy modification.
+
+| Option                     | Default                         | Notes                                                                                                                                                                         |
+|----------------------------|---------------------------------|-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| SERIAL_PORT                | /dev/ttyACM0                    | Set to match the port where your device is connected.                                                                                                                         |
+| SERIAL_BAUD                | 115200                          | Set to match your device's baudrate. Ignored for USB.                                                                                                                         |
+| LOGICAL_GPIO_PIN           | 1                               | Set to the logical (as opposed to physical connector pin) GPIO pin number (e.g. 1-4).                                                                                         |
+| TRIGGER_ID                 | 1                               | Selects which event trigger slot to use.                                                                                                                                      |
+| ACTION_ID                  | 1                               | Selects which event action slot to use.                                                                                                                                       |
+| FREQUENCY                  | 60                              | Sets the square wave frequency in Hz.                                                                                                                                         |
+| DUTY_CYCLE                 | 0.5                             | Sets the square wave duty cycle. 50% --> 0.5, 10% --> 0.1, etc. The minimum period is 1ms (shorter periods will only work intermittently).                                    |
+| TIMESTAMP_DESCRIPTOR_SET   | 0x80 Sensor Data                | Controls which descriptor set the time is sourced from. Can be 0x80 Sensor Data, 0x82 Filter Data, 0xA0 System Data. GNSS Data sets are not supported (use system time instead). |
+| TIMESTAMP_FIELD_DESCRIPTOR | 0xD5 Internal Reference         | Controls the time reference frame is used for the square wave. Can be 0xD5 Shared Reference Time, 0xD7 Shared External Time, or 0xD3 Shared GPS Time (same as External Time). |
+| TIMESTAMP_PARAMETER        | 1 (Nanoseconds)                 | Which field from the timestamp contains the time. For all 3 fields mentioned above, parameter 1 holds the actual time quantity.                                               |
+| TIMESTAMP_INTERVAL         | 1.0 / FREQUENCY                 | [ADVANCED] Period of the square wave in units of the selected time value. For Reference and External time, this is nanoseconds. For GPS TOW, this is seconds.                 |
+| TIMESTAMP_THRESHOLD        | DUTY_CYCLE * TIEMSTAMP_INTERVAL | [ADVANCED] Threshold of the square wave in the same units as the interval. The pin will be HIGH when time is between the start of the interval and this (relative) threshold. |
+
+
+Valid Time Sources
+------------------
+
+### Descriptor Sets
+
+* 0x80 Sensor Data - `mip::data_sensor::DESCRIPTOR_SET`
+* 0x82 Filter Data - `mip::data_filter::DESCRIPTOR_SET`
+* 0xA0 System Data - `mip::data_system::DESCRIPTOR_SET`
+
+Note: Threshold triggers do not support GNSS data quantities on CV7.
+
+### Shared Timestamp Quantities
+
+See [Synchronization Overview](https://s3.amazonaws.com/files.microstrain.com/CV7+Online/user_manual_content/app_notes/Synchronization%20Overview.htm)
+for a description of the differences between these quantities.
+
+* [0xD5 Reference Timestamp](https://s3.amazonaws.com/files.microstrain.com/CV7+Online/external_content/dcp/Data/0xff/data/0xd5.htm)
+* [0xD7 External Timestamp](https://s3.amazonaws.com/files.microstrain.com/CV7+Online/external_content/dcp/Data/0xff/data/0xd7.htm)
+* [0xD3 GPS Timestamp](https://s3.amazonaws.com/files.microstrain.com/CV7+Online/external_content/dcp/Data/0xff/data/0xd3.htm)
+  Note: For CV7, this is the same as the External Timestamp, just formatted as GPS Time.
+  If you select this option, the interval and threshold units must be changed from
+  nanoseconds to seconds.
+
---
+++ square_wave_output/main.cpp
@@ -0,0 +1,373 @@
+
+#include <microstrain/logging.hpp>
+#include <microstrain/connections/serial/serial_connection.hpp>
+
+#include <mip/mip_interface.hpp>
+#include <mip/definitions/commands_base.hpp>
+#include <mip/definitions/commands_3dm.hpp>
+#include <mip/definitions/data_sensor.hpp>
+#include <mip/definitions/data_filter.hpp>
+#include <mip/definitions/data_system.hpp>
+#include <mip/definitions/data_gnss.hpp>
+#include <mip/definitions/data_shared.hpp>
+
+#include <cstdio>
+#include <csignal>
+#include <thread>
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Configuration
+//
+////////////////////////////////////////////////////////////////////////////////
+
+// This is the port used to communicate with the device.
+static constexpr const char* const SERIAL_PORT = "/dev/ttyACM0";
+
+// This should match the device's baud rate.
+static constexpr uint32_t    SERIAL_BAUD = 115200;
+
+// The square wave will be output on this logical GPIO pin number.
+static constexpr uint8_t LOGICAL_GPIO_PIN = 1;
+
+// Trigger instance number.
+static constexpr uint8_t TRIGGER_ID = 1;
+
+// Action instance number.
+static constexpr uint8_t ACTION_ID  = 1;
+
+// Square wave frequency in Hz.
+static constexpr double FREQUENCY = 60.0;
+
+// Square wave duty cycle in the range 0 to 1.
+// E.g. 50% --> 0.5 or 10% --> 0.1
+static constexpr double DUTY_CYCLE = 0.5;
+
+
+// The square wave will be synchronized with the timestamp from this descriptor set.
+//static const uint8_t TIMESTAMP_DESCRIPTOR_SET = mip::data_sensor::DESCRIPTOR_SET;
+//static const uint8_t TIMESTAMP_DESCRIPTOR_SET = mip::data_filter::DESCRIPTOR_SET;
+static constexpr uint8_t TIMESTAMP_DESCRIPTOR_SET = mip::data_system::DESCRIPTOR_SET;
+
+// Select one of these options:
+// - Reference Time [default]: Internal reference time in nanoseconds.
+// - External Time:            External synchronized time (e.g. GNSS) in nanoseconds.
+// - GPS Time:                 Same as external time, but with time of week in seconds. Requires changing threshold (see below).
+static constexpr uint8_t TIMESTAMP_FIELD_DESCRIPTOR = mip::data_shared::DATA_REFERENCE_TIME;
+//static const uint8_t TIMESTAMP_FIELD_DESCRIPTOR = mip::data_shared::DATA_EXTERNAL_TIME;
+//static const uint8_t TIMESTAMP_FIELD_DESCRIPTOR = mip::data_shared::DATA_GPS_TIME;
+
+// Selects which parameter in the corresponding timestamp field is used for comparison.
+// For reference and external timestamps, the first parameter is nanoseconds.
+// For GPS time, Time of Week (TOW) is the first parameter.
+static constexpr uint8_t TIMESTAMP_PARAMETER = 1;
+
+// Units for the timestamp field. This is only used for the math in this section.
+// You may ignore this value if setting TIMESTAMP_INTERVAL yourself (in which case,
+// you may also need to disable some of the validation checks if they complain).
+static constexpr double TIMESTAMP_UNITS = 1.0e-9;  // Nanoseconds, for Internal or External Timestamps.
+//static constexpr double TIMESTAMP_UNITS = 1.0;     // Seconds, for GPS Time.
+
+// This value is what actually controls the period of the square wave.
+// The units are determined by the specific timestamp field chosen.
+// By default, and for explanatory purposes, it is computed from the FREQUENCY and
+// TIMESTAMP_UNITS values, but you may override that logic if you wish to set it directly.
+// If you do set this parameter manually, FREQUENCY will no longer be used.
+// You may still need to set TIMESTAMP_UNITS so that the validation assertions don't complain.
+static constexpr double TIMESTAMP_INTERVAL  = 1.0 / (TIMESTAMP_UNITS * FREQUENCY);
+
+// This value is what actually controls the duty cycle. The pin will be HIGH
+// when the time is between the start of the interval and this value (relative to the start of the interval).
+// Duty cycle = threshold / interval.
+// If you set this parameter manually, DUTY_CYCLE will no longer be used.
+static constexpr double TIMESTAMP_THRESHOLD = DUTY_CYCLE * TIMESTAMP_INTERVAL;
+
+////////////////////////////////////////////////////////////////////////////////
+
+//
+// Basic sanity checks on parameters.
+//
+
+static_assert(LOGICAL_GPIO_PIN >= 1 && LOGICAL_GPIO_PIN <= 4, "LOGICAL_GPIO_PIN must be within [1,4] for CV7.");
+static_assert(TRIGGER_ID >= 1 && TRIGGER_ID <= 12, "TRIGGER_ID must be within [1,12] for CV7.");
+static_assert(ACTION_ID >= 1 && ACTION_ID <= 12, "ACTION_ID must be within [1,12] for CV7.");
+static_assert(FREQUENCY <= 1000.0, "Maximum frequency is 1000 Hz for CV7.");
+static_assert(DUTY_CYCLE >= 0 && DUTY_CYCLE <= 1.0, "DUTY_CYCLE must be between 0 and 1.");
+static_assert(TIMESTAMP_INTERVAL*TIMESTAMP_UNITS > 0.001, "TIMESTAMP_INTERVAL must be at least 1 ms.");
+static_assert(TIMESTAMP_INTERVAL*TIMESTAMP_UNITS < 1000, "TIMESTAMP_INTERVAL is > 1000 s, this is probably not what you wanted. Check the units.");
+static_assert(TIMESTAMP_THRESHOLD*TIMESTAMP_UNITS > 0.001, "TIMESTAMP_THRESHOLD must be at least 1 ms.");
+static_assert(TIMESTAMP_THRESHOLD < TIMESTAMP_INTERVAL, "TIMESTAMP_THRESHOLD must be less than TIMESTAMP_INTERVAL.");
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+bool showTriggerStatus(mip::Interface& device, bool fullDisplay=true);
+
+void log_callback(void*, const microstrain_log_level level, const char* fmt, va_list args)
+{
+    // Print log messages stdout using the
+    std::printf("[%s]: ", microstrain_logging_level_name(level));
+    std::vprintf(fmt, args);
+}
+
+int main()
+{
+    //
+    // DEVICE SETUP
+    //
+
+    // Initialize logging so messages can be printed.
+    // Change the log level if you want to see more- or less-detailed messages.
+    MICROSTRAIN_LOG_INIT(log_callback, MICROSTRAIN_LOG_LEVEL_TRACE, nullptr);
+
+    // Create the connection. This does not open the port yet.
+    microstrain::connections::SerialConnection connection(SERIAL_PORT, SERIAL_BAUD);
+
+    // Open the port now.
+    if(!connection.connect())
+    {
+        MICROSTRAIN_LOG_FATAL("Could not open serial port '%s'\n", connection.interfaceName());
+        return 1;
+    }
+
+
+    const uint32_t PARSE_TIMEOUT = 100;  // Time for one mip packet to be parsed before timing out.
+    const uint32_t REPLY_TIMEOUT = 1000; // Max turnaround time between sending a command and expecting the reply.
+
+    // Create the device interface using the specified connection.
+    mip::Interface device(&connection, PARSE_TIMEOUT, REPLY_TIMEOUT);
+
+    // Set the device to idle and test communications.
+    MICROSTRAIN_LOG_DEBUG("Testing communication with the device\n");
+    mip::CmdResult result = mip::commands_base::setIdle(device);
+    if(result == mip::CmdResult::STATUS_TIMEDOUT)
+    {
+        // If the device is streaming at a high rate,
+        // the reply might get lost. Try again.
+        result = mip::commands_base::setIdle(device);
+    }
+    // Check if result was NACK or failed the second time also.
+    if(!result)
+    {
+        MICROSTRAIN_LOG_FATAL("Failed to set the device to idle: %d %s\n", result.value, result.name());
+        return 1;
+    }
+
+    // Reset to default settings.
+    //result = mip::commands_3dm::defaultDeviceSettings(device);
+    //if(!result)
+    //{
+    //    MICROSTRAIN_LOG_FATAL("Failed to reset to default settings: %d %s\n", result.value, result.name());
+    //    return 1;
+    //}
+    result = mip::commands_3dm::defaultGpioConfig(device, 0);
+    if(!result)
+    {
+        MICROSTRAIN_LOG_FATAL("Failed to reset all GPIO pins: %d %s\n", result.value, result.name());
+        return 1;
+    }
+    result = mip::commands_3dm::defaultEventTrigger(device, 0);
+    if(!result)
+    {
+        MICROSTRAIN_LOG_FATAL("Failed to reset event triggers: %d %s\n", result.value, result.name());
+        return 1;
+    }
+    result = mip::commands_3dm::defaultEventAction(device, 0);
+    if(!result)
+    {
+        MICROSTRAIN_LOG_FATAL("Failed to reset event actions: %d %s\n", result.value, result.name());
+        return 1;
+    }
+
+    // Show that the trigger is not enabled nor active yet.
+    // This is just for demonstration purposes and is completely optional.
+    MICROSTRAIN_LOG_INFO("Trigger status after setting factory defaults:\n");
+    showTriggerStatus(device);
+
+    //
+    // Gpio Setup
+    //
+
+    // Configure the pin to GPIO OUTPUT_LOW.
+    // This will take immediate effect.
+
+    result = mip::commands_3dm::writeGpioConfig(
+        /* device   */ device,
+        /* pin      */ LOGICAL_GPIO_PIN,
+        /* Feature  */ mip::commands_3dm::GpioConfig::Feature::GPIO,
+        /* Behavior */ mip::commands_3dm::GpioConfig::Behavior::GPIO_OUTPUT_LOW,
+        /* Mode     */ mip::commands_3dm::GpioConfig::PinMode::NONE
+    );
+
+    if(!result)
+    {
+        MICROSTRAIN_LOG_FATAL("Failed to configure gpio pin %u: %d %s\n", LOGICAL_GPIO_PIN, result.value, result.name());
+        return 1;
+    }
+
+    //
+    // Event Action Setup
+    //
+
+    // Configures an event action to set the pin HIGH while the trigger is activated, and LOW otherwise.
+
+    // We use the command struct instead of the write function for clarity.
+    // It would also be perfectly fine to use mip::commands_3dm::writeEventAction, etc. instead.
+
+    mip::commands_3dm::EventAction action;
+    action.function = mip::FunctionSelector::WRITE;
+    action.instance = ACTION_ID;
+    action.trigger  = TRIGGER_ID;
+    action.type     = mip::commands_3dm::EventAction::Type::GPIO;
+    action.parameters.gpio.pin  = LOGICAL_GPIO_PIN;
+    action.parameters.gpio.mode = mip::commands_3dm::EventAction::GpioParams::Mode::ACTIVE_HIGH;
+
+    result = device.runCommand(action);
+
+    if(!result)
+    {
+        MICROSTRAIN_LOG_FATAL("Failed to configure gpio action in slot %u: %d %s\n", ACTION_ID, result.value, result.name());
+        return 1;
+    }
+
+    //
+    // Event Trigger Setup
+    //
+
+    mip::commands_3dm::EventTrigger trigger;
+    trigger.function = mip::FunctionSelector::WRITE;
+    trigger.instance = TRIGGER_ID;  // This must match the action's trigger ID.
+    trigger.type     = mip::commands_3dm::EventTrigger::Type::THRESHOLD;
+    trigger.parameters.threshold.desc_set   = TIMESTAMP_DESCRIPTOR_SET;
+    trigger.parameters.threshold.field_desc = TIMESTAMP_FIELD_DESCRIPTOR;
+    trigger.parameters.threshold.param_id   = TIMESTAMP_PARAMETER;
+    trigger.parameters.threshold.type       = mip::commands_3dm::EventTrigger::ThresholdParams::Type::INTERVAL;
+    trigger.parameters.threshold.int_thres  = TIMESTAMP_THRESHOLD;
+    trigger.parameters.threshold.interval   = TIMESTAMP_INTERVAL;
+
+    result = device.runCommand(trigger);
+
+    if(!result)
+    {
+        MICROSTRAIN_LOG_FATAL("Failed to configure threshold trigger in slot %u: %d %s\n", TRIGGER_ID, result.value, result.name());
+        return 1;
+    }
+
+    // Show that the trigger is not enabled nor active yet.
+    // This is just for demonstration purposes and is completely optional.
+    MICROSTRAIN_LOG_INFO("Trigger status before trigger is enabled:\n");
+    showTriggerStatus(device);
+
+    //
+    // Enable The Trigger
+    //
+
+    result = mip::commands_3dm::writeEventControl(
+        /* device   */ device,
+        /* instance */ TRIGGER_ID,
+        /* mode     */ mip::commands_3dm::EventControl::Mode::ENABLED
+    );
+
+    if(!result)
+    {
+        MICROSTRAIN_LOG_FATAL("Failed to enable the trigger in slot %u: %d %s\n", TRIGGER_ID, result.value, result.name());
+        return 1;
+    }
+
+    //
+    // DONE
+    //
+
+    MICROSTRAIN_LOG_INFO("Configuration complete!\n");
+
+    //
+    // Extra: Monitor the state of the trigger for demonstration/debugging purposes.
+    //
+
+    // Setup a signal handler to catch ctrl+C.
+    static bool stop = false;
+    std::signal(SIGTERM, [](int){ stop=true; });
+
+    if(FREQUENCY <= 5.0)
+    {
+        while(!stop)
+        {
+            bool ok = showTriggerStatus(device);
+            if(!ok)
+                return 1;
+
+            // 10 Hz updates
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }
+    else // High frequency
+    {
+        MICROSTRAIN_LOG_INFO("Trigger status stream (+ = active, - = inactive):\n");
+
+        auto sampleTime = std::chrono::system_clock::now();
+
+        // Run for 10 seconds, printing a + or - every 10 milliseconds depending on the trigger state.
+        // Each line has 100 samples.
+        for(unsigned int i=0; i<10'000; i++)
+        {
+            std::this_thread::sleep_until(sampleTime);
+            sampleTime += std::chrono::milliseconds(1);
+
+            showTriggerStatus(device, false);
+
+            // Newline every 100 samples.
+            if(i % 100 == 99)
+            {
+                if( microstrain_logging_level() >= MICROSTRAIN_LOG_LEVEL_INFO )
+                    putchar('\n');
+            }
+        }
+    }
+
+    return 0;
+}
+
+bool showTriggerStatus(mip::Interface& device, bool fullDisplay)
+{
+    mip::commands_3dm::GetEventTriggerStatus statusCmd;
+    statusCmd.requested_instances[0] = TRIGGER_ID;
+    statusCmd.requested_count = 1;
+    mip::commands_3dm::GetEventTriggerStatus::Response statusRsp;
+
+    mip::CmdResult result = device.runCommand(statusCmd, statusRsp);
+
+    if(!result)
+    {
+        MICROSTRAIN_LOG_FATAL("Failed to get trigger status: %d %s\n", TRIGGER_ID, result.value, result.name());
+        return false;
+    }
+
+    if(statusRsp.count != 1)
+    {
+        MICROSTRAIN_LOG_FATAL("Unexpected count %u for trigger status response\n", statusRsp.count);
+        return false;
+    }
+
+    if(fullDisplay)
+    {
+        const char active  = statusRsp.triggers[0].status.active()  ? 'X' : ' ';
+        const char enabled = statusRsp.triggers[0].status.enabled() ? 'X' : ' ';
+        const char testing = statusRsp.triggers[0].status.test()    ? 'X' : ' ';
+
+        // Display all 3 status parameters
+        MICROSTRAIN_LOG_INFO("Trigger %u status:  [%c] Active  [%c] Enabled  [%c] Testing\n", statusCmd.requested_instances[0], active, enabled, testing);
+    }
+    else
+    {
+        const char active  = statusRsp.triggers[0].status.active()  ? '+' : '-';
+
+        // Just print an X or space with no newline, for high update speed.
+        if(microstrain_logging_level() >= MICROSTRAIN_LOG_LEVEL_INFO)
+            putchar(active);
+    }
+
+    return true;
+}
